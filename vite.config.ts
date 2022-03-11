e8dab46c9ddefc91eeeb64dc78569c39f21a23c9
TED Vortex <ted.vortex@gmail.com>
2022-03-12 00:24:46 +0200

    test: test rebase branch

--- vite.config.ts
+++ vite.config.ts
@@ -121,12 +121,6 @@ export default defineConfig(({command, mode}: ConfigEnv): UserConfig => {
 
   // deploy preview build options
   if (isNetlifyBuild) {
-    // leave this section commented to test out custom domains later on
-    console.log(`process.env.CHANNEL: `, process.env.CHANNEL);
-    console.log(`process.env.URL: `, process.env.URL);
-    console.log(`process.env.DEPLOY_URL: `, process.env.DEPLOY_URL);
-    console.log(`process.env.DEPLOY_PRIME_URL: `, process.env.DEPLOY_PRIME_URL);
-
     config.base = `${process.env.URL}/`;
 
     if (isNetlifyPreviewBuild) {
@@ -136,7 +130,6 @@ export default defineConfig(({command, mode}: ConfigEnv): UserConfig => {
       // leaving it like this to be extended or simplified
       if (isNetlifyChannelBuild && ['alpha', 'beta'].includes(process.env.CHANNEL)) {
         const {protocol, hostname} = new URL(process.env.URL);
-        console.log(process.env.BRANCH);
         config.base = `${protocol}//${process.env.CHANNEL}.${hostname}/`;
       }
     }
